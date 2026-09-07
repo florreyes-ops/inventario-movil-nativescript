45373bc9930d13c51f910115160b187f13db589c
florreyes-ops <flor.reyes@ukuepa.com>
2026-09-07 01:52:14 -0600

    Agregar FormsModule
    Se habilita ngModel para el formulario de búsqueda.

--- src/app/productos/productos.module.ts
+++ src/app/productos/productos.module.ts
@@ -1,12 +1,21 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { NativeScriptCommonModule } from '@nativescript/angular';
+
 import { ProductosRoutingModule } from './productos-routing.module';
 import { ProductosListaComponent } from './productos-lista/productos-lista.component';
 import { ProductoDetalleComponent } from './producto-detalle/producto-detalle.component';
 
 @NgModule({
-  imports: [NativeScriptCommonModule, ProductosRoutingModule],
-  declarations: [ProductosListaComponent, ProductoDetalleComponent],
+  imports: [
+    NativeScriptCommonModule,
+    FormsModule,
+    ProductosRoutingModule
+  ],
+  declarations: [
+    ProductosListaComponent,
+    ProductoDetalleComponent
+  ],
   schemas: [NO_ERRORS_SCHEMA]
 })
 export class ProductosModule {}
